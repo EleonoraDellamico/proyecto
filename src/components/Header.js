1993de54f6767d0f50b7de27065fe25414762543
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2021-02-16 17:36:23 +0100

    changed header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,14 @@
+import Video from '../img/videoHeader.mp4';
+
 function Header() {
 	return (
 		<div className="header">
+			<video muted autoPlay loop className="video">
+				<source src={Video} />
+			</video>
 			<div className="header-text-animation">
 				<p className="text-animated">I'm Eleonora</p>
 				<p className="text-animated">Junior Frontend</p>
-			</div>
-			<div className="header-text">
-				<p className="text">
-					I use react to develop pages.
-					<span>
-						This project is a resume template that can be used as the project home page or resume page.
-					</span>
-				</p>
 			</div>
 		</div>
 	);
