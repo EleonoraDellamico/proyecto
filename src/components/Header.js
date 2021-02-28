25af5bdbf14d035792f82b52e744bfdaecfea028
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2021-02-28 22:49:51 +0100

    changed background header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,10 @@
 import Video from '../img/yellow.mp4';
 import Btn from '../img/btn.png';
-import PhotoFear from '../img/Fearless.jpg';
+// import PhotoFear from '../img/Fearless.jpg';
 
 function Header() {
 	return (
 		<div className="header">
-			<div className="container__photoHeader">
-				<img src={PhotoFear} alt="motivational" />
-			</div>
 			<div className="header-text-animation">
 				<p className="text-animated">I'm Eleonora</p>
 				<p className="text-animated">Junior Frontend</p>
